6b52e999ecf336d6b089c9e3a7d0d7ca261747ae
BriBriMurray <loveandintention1@gmail.com>
2024-10-22 19:08:58 -0400

    created files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const url = require('url');
 let addr = 'http://localhost:8080/default.html?year=2017&month=february';
 let q = new URL(addr,  'http://localhost:8080');
 
+
+
 console.log(q.host); // returns 'localhost:8080'
 console.log(q.pathname); // returns '/default.html'
 console.log(q.search); // returns '?year=2017&month=february'
